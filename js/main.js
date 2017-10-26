f43e796ce634a7a9d09383115dfe8775d5509ba7
ValentinkaV <31902735+ValentinkaV@users.noreply.github.com>
2017-10-26 13:20:25 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,112 +1,152 @@
-"use strict"
+"use strict";
 
 function Device(name, brand, location) {
 	this.name = name;
 	this.brand = brand;
 	this.location = location;
-	var _state = false;
+}
 
-	this.switchState = function () {
-		querySelector('[type="checkbox"]').addEventListener('change', function() {
-			console.log(this.checked);
-			_state = this.checked;
-		});
+// //мультиварка
+// function Multicooker(name, brand, location, program, delayStart) {
+// 	Device.call(this, name, brand, location);
+// 	this.program = [{
+// 		name: "Baking",
+// 		temperature: "170",
+// 		cookingTime: 3000
+// 	}, {
+// 		name: "Аrying",
+// 		temperature: "130",
+// 		cookingTime: 200
+// 	}];
+// 	this._delayStart = delayStart;
+// 	this.getProgram = function() {
+// 		return this._program;
+// 	};
+// 	this.setProgram = function() {
+// 		this._program = program;
+// 	};
+// 	this.getDelayStart = function() {
+// 		return this._delayStart;
+// 	};
+// 	this.setDelayStart = function() {
+// 		this._delayStart = delayStart;
+// 	};
+// }
+
+
+// // // кондиционер
+// function Conditioner(name, brand, location, temperature) {
+// 	Device.call(this, name, brand, location);
+// 	var step = 0;
+// 	var maxStep = 5;
+// 	this.setNextStep = function() {
+// 		step++;
+// 		if(step > maxStep) {
+// 			step = 0;
+// 		}
+// 	};
+// 	this.temperature = 22;
+// 	this.minTemperature = 0;
+// 	this.maxTemperature = 35;
+// }
+
+// // // стиральная машина
+// function WashingMachine(name, brand, location, delayStart, timeOff) {
+// 	Device.call(this, name, brand, location);
+// 	this.delayStart = delayStart;
+// 	this.timeOff = timeOff;
+// 	this.getDelayStart = function() {
+// 		return this._delayStart;
+// 	};
+// 	this.setDelayStart = function() {
+// 		this._delayStart = program;
+// 	};
+// 	this.getTimeOff = function() {
+// 		return this._timeOff;
+// 	};
+// 	this.setTimeOff = function() {
+// 		this._timeOff = timeOff;
+// 	};
+// }
+
+// // // телевизор
+// function Tv(name, brand, location, volume, channel) {
+// 	Device.call(this, name, brand, location);
+// 	this.channel = channel;
+// 	this.volume = 0;
+// 	this.nextChannel = function() {
+// 			tv.nextChannel = ++tv.channel
+// 		},
+// 		this.previousChannel = function() {
+// 			tv.previousChannel = --tv.channel
+// 		},
+// 		this.setChannel = function(number) {
+// 			this.setChannel = number
+// 		}
+// 	this.setVolume = function(volume) {
+// 		if (volume >= 0 && volume <= 100) {
+// 			_volume = volume;
+// 		}
+// 	};
+// 	this.getVolume = function() {
+// 		return _volume;
+// 	};
+// }
+
+var DevObj = {
+	"tv": {
+		name: "tv",
+		brand: "Samsung",
+		location:  "Kitchen",
+		volume: 20,
+		channel: 30
+	},
+	"conditioner": {
+		name: "cond",
+		brand: "Samsung",
+		location:  "Kitchen",
+		temperature: 24
+	},
+	"multicooker": {
+		name: "multicooker",
+		brand: "Tefal",
+		location:  "Kitchen",
+		program: "baking",
+		delayStart: 3000
+	},
+	"washingmachine": {
+		name: "Washing Machine",
+		brand: "Samsung",
+		location:  "Bathroom",
+		delayStart: 3000,
+		timeOff: 18
 	}
-}
+};
 
-//мультиварка
-function Multicooker(name, brand, location, program) {
-	Device.call(this, name, brand, location);
-	// this.program = [{
-	// 	name: "Baking",
-	// 	temperature: "170",
-	// 	cookingTime: 3000
-	// }, {
-	// 	name: "Аrying",
-	// 	temperature: "130",
-	// 	cookingTime: 200
-	// }];
-	// this.getProgram = function() {
-	// 	return this._program;
-	// };
-	// this.setProgram = function() {
-	// 	this._program = program;
-	// };
-	this.program = program;
-}
-var multicooker = new Multicooker('Multicooker', 'Tefal', 'Kitchen', "Baking");
+var addDev = document.querySelectorAll(".btn-add");
 
+for (var i = 0; i < addDev.length; i++) {
+	addDev[i].addEventListener('click', function(e) {
+		e.preventDefault();
+		addDevice(this);
+	});
+};
 
-// // кондиционер
-function Conditioner(name, brand, location, temperature) {
-	Device.call(this, name, brand, location);
-	var _temperature = 10;
-	this.setTemperature = function(temperature) {
-		if (temperature >= 0 && volume <= 35) {
-			_temperature = temperature;
-		}
-	};
-	this.getTemperature = function() {
-		return _temperature;
-	};
-}
-var conditioner = new Conditioner('Conditioner', 'Samsung', 'Common room', 22);
+var results = document.getElementById("results");
 
-// // аудиоплеер
-function Player(name, brand, location, volume) {
-	Device.call(this, name, brand, location);
-	this.volume = 0;
-	this.setVolume = function(volume) {
-		if (volume >= 0 && volume <= 100) {
-			_volume = volume;
-		}
-	};
-	this.getVolume = function() {
-		return _volume;
-	};
-}
-var player = new Player('Audio player', 'Sony', 'Common room', 20);
-
-
-function SmartHouse() {
-	var _devices = [];
-
-	this.addDevice = function(device) {
-		device = _devices.length;
-		_devices.push(device);
-	};
-
-	this.delDeviceItem = function(item) {
-		for(var i = 0; i < _devices.length; i++) {
-			if(_devices[i].item == item) {
-				_devices.splice(i,1);
-				return;
-			}
-		}
-	};
-
-	this.getDevices = function () {
-		var devices = _devices.slice();
-		return devices;
-	};
-}
-
-var results = document.getElementById("dev-holder");
-
-function AddingToSmartHouse() {
+function addDevice(button) {
 	var idElem = button.getAttribute("id");
 	var divDev = document.createElement("div");
-	divDev.className = "dev-box";
+	divDev.className = "controler-dev-box";
 	results.appendChild(divDev);
 
-	var deviceType = divDev.querySelector('.title');
-	deviceName.innerHTML = device.deviceName;
+	var templateBox = document.getElementById('template');
+	var source = templateBox.innerHTML;
+	var template = Handlebars.compile(source);
 
-	var deviceName = divDev.querySelector('.brand');
-	deviceBrand.innerHTML = device.deviceBrand;
+	DevObj[idElem].id = Date.now();
 
-	var devicelocation = divDev.querySelector('.location');
-	deviceLocation.innerHTML = deviceLocation;
+	divDev.innerHTML = template(DevObj[idElem]);
 
 	// удаление блока
 	var btnDelete = divDev.querySelector('.danger');
@@ -115,10 +155,27 @@ function AddingToSmartHouse() {
 		e.preventDefault();
 		divDev.remove();
 	});
+
+	var btnApply = divDev.querySelectorAll(".btn-apply");
+	var devValue = document.createElement("span");
+	devValue.className = "value";
+	divDev.appendChild(devValue);
+
+	for (var i = 0; i < btnApply.length; i++) {
+		btnApply[i].addEventListener('click', function(e) {
+			e.preventDefault();
+			var parent = this.closest('.add-value');
+			var field = parent.querySelectorAll('input, select')[0];
+			var valueField = parent.querySelector('.value');
+
+			valueField.innerHTML = field.value;
+		});
+	}
+
+	var state = false;
+	divDev.querySelector('[type="checkbox"]').addEventListener('change', function() {
+		console.log(this.checked);
+		state = this.checked;
+	});
 }
 
-
-var SmartHouse = new SmartHouse();
-// SmartHouse.addDevice(player);
-// SmartHouse.addDevice(conditioner);
-// SmartHouse.addDevice(multicooker);
